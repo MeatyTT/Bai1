8617f3173fa15c8c7fc218e23cf2e9ce6e3bf85d
MeatyTT <letrieutuan94@gmail.com>
2021-09-26 23:18:17 +0700

    commit

--- BVN2/index.js
+++ BVN2/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const port = 3000;
+const port = 1210;
 app.use(express.static('anh'));
 
 app.use('/',router);
