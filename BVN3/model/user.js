dedc7e08eedf550a7916c668fd4c282cf3536cee
MeatyTT <letrieutuan94@gmail.com>
2021-09-30 21:55:11 +0700

    commit

---
+++ BVN3/model/user.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+const path = require('path')
+
+const addNewPromise = async student => {
+    try {
+        const isExit = await fs.existsSync('student.json');
+        if (!isExit) {
+            const newList = [student];
+            const dataToSave = JSON.stringify(newList)
+            await fs.promises.writeFile('student.json', dataToSave);
+            return 1;
+        }
+        const data = await fs.promises.readFile(path.resolve(__dirname, 'student.json'), 'utf8');
+        const dataConvert = JSON.parse(data);
+        const newList = [...dataConvert, student];
+
+        await fs.promises.writeFile(path.resolve(__dirname, 'student.json'), JSON.stringify(newList));
+        console.log('Thêm user mới thành công');
+
+    } catch (err) {
+        console.log('Thêm user mới thất bại');
+        throw err;
+
+    }
+}
+const getAllUser = async () => {
+    const result = await fs.promises.readFile(path.resolve(__dirname, 'student.json'), 'utf8')
+    const dataConvert = JSON.parse(result)
+    return dataConvert;
+}
+const queryUser = async (params) => {
+    const allUser = await getAllUser();
+    const result = allUser.filter(user => {
+        if (params.name) {
+            user.name.includes(params.name) || params.age == user.age;
+        } else {
+            return params.age == user.age;
+        }
+    });
+    return result;
+}
+const deleteUserById = async (userId) => {
+    try {
+        const allUser = await getAllUser();
+        const newListUser = allUser.filter(user => {
+            return user.id != userId
+        })
+        if (newListUser.length === allUser.length) {
+            return {
+                status: 400,
+                msg: `Không tìm thấy user ${userId} `
+            }
+        } else {
+            await fs.promises.writeFile(path.resolve(__dirname, 'student.json'), JSON.stringify(newListUser));
+            return {
+                status: 200,
+                msg: `Xóa bỏ user ${userId} thành công`
+            }
+        }
+        return 1;
+    } catch (err) {
+        throw err
+    }
+}
+const writeFile = async (content) => {
+    await fs.promises
+        .writeFile(path.resolve(__dirname, 'student.json'), JSON.stringify(content))
+        .catch((error) => {
+            if (error) throw error;
+        });
+};
+const check = async (userID) => {
+    const read = await fs.promises.readFile(path.resolve(__dirname, 'student.json'), "utf8");
+
+    let list = JSON.parse(read);
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].id == userID) {
+            return true;
+        }
+    }
+};
+const updateUserById = async (userId, body) => {
+    const data =  await fs.promises.readFile(path.resolve(__dirname,'student.json'),'utf8');
+    let list = JSON.parse(data);
+
+    for(var i = 0; i < list.length; i++) {
+        if(list[i].id == userId){
+            console.log("tim thay data");
+            list[i] = body;
+            console.log(body);
+        }
+        // console.log(list);
+    }
+    console.log(1);
+    fs.writeFileSync(path.resolve(__dirname, 'student.json'), JSON.stringify(list));
+}
+
+const updateUser = async (body) => {
+    try {
+        fs.writeFileSync(path.resolve(__dirname, 'student.json'), JSON.stringify(body));
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = {
+    getAllUser,
+    queryUser,
+    addUser: addNewPromise,
+    deleteUserById,
+    updateUserById,
+    updateUser,
+}
---
+++ BVN3/router/user.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const { getAllUser, queryUser, addUser, deleteUserById, updateUserById, updateUser } = require('../model/user')
+
+let userRouter = express.Router();
+
+userRouter.get('/', async (req, res) => {
+    try {
+
+        // console.log(req.query) // ?name1=val&name2=sdaf&asdf
+        const { name = '', age = '' } = req.query || {};
+        //lấy toàn bộ user
+        if (!name && !age) {
+            console.log(req.query, 'query');
+            const result = await getAllUser();
+            res.json(result);
+            return 1;
+        } else {
+            const result = await queryUser({ name, age });
+            res.json(result);
+            return 1;
+        }
+        // const name=req.query.name;
+        // const age=req.query.age;
+
+        // res.send('get method: ' + req.params.user)
+    } catch (err) {
+        console.log('error get user', err);
+        res.status(500).json({
+            msg: err
+        })
+    }
+});
+
+userRouter.post('/', async (req, res) => {
+    try {
+        const body = req.body;
+        console.log('post', body);
+        await addUser(body);
+        res.json({
+            msg: 'Thêm mới user thành công'
+        });
+    } catch (err) {
+        console.log('error post user', err);
+        res.status(500).json({
+            msg: err
+        })
+    }
+
+    // res.send('post method: ' + req.params.user)
+});
+
+userRouter.patch('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const body = req.body;
+        await updateUserById(userId,body)
+        res.json({
+            msg:`cập nhật user ${userId} thành công`
+        })
+    } catch (err) {
+        console.log('error patch user');
+        res.status(500).json({
+            msg: err
+        })
+    }
+});
+
+userRouter.put('/', async (req, res) => {
+    try {
+        const body = req.body;
+        await updateUser(body);
+        res.json({
+            msg: 'Tạo mới danh sách user thành công'
+        });
+    } catch (err) {
+        console.log('error put user');
+        res.status(500).json({
+            msg: err
+        })
+    }
+});
+
+userRouter.delete('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const result = await deleteUserById(userId)
+        res.status(result.status).json({
+            msg: result.msg
+        })
+
+    } catch (err) {
+        console.log('error delete user');
+        res.status(500).json({
+            msg: err
+        })
+    }
+});
+
+module.exports = userRouter;
